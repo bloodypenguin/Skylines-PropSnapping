270ac0e406812a51ca3e7570c2bb79517c243d5e
earalov <earalov@thumbtack.net>
2016-10-31 10:32:14 +0600

    Disable in editors

--- PropSnapping/LoadingExtension.cs
+++ PropSnapping/LoadingExtension.cs
@@ -3,13 +3,25 @@ using PropSnapping.Redirection;
 
 namespace PropSnapping
 {
-    public class LoadingExctension : LoadingExtensionBase
+    public class LoadingExtension : LoadingExtensionBase
     {
         public override void OnCreated(ILoading loading)
         {
             base.OnCreated(loading);
-            Redirector<PropToolDetour>.Deploy();
             Redirector<PropInstanceDetour>.Deploy();
+        }
+
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            base.OnLevelLoaded(mode);
+            if (mode == LoadMode.LoadGame || mode == LoadMode.NewGame)
+            {
+                Redirector<PropToolDetour>.Deploy();
+            }
+            else
+            {
+                Redirector<PropInstanceDetour>.Revert();
+            }
         }
 
         public override void OnReleased()
